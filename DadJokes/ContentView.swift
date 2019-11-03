3c3921018e571d838db2efca4d04cf013a95073e
Murillo R. Araujo <mr_araujo@icloud.com>
2019-11-03 15:53:58 -0300

    Initial Commit

---
+++ DadJokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJokes
+//
+//  Created by Murillo R. Araujo on 03/11/19.
+//  Copyright © 2019 Code In Capsule. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
